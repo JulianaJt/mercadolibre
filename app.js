127983da2f8ede50059738469c6faec52beab277
JulianaJt <juliana.trujillov@urosario.edu.co>
2022-09-03 23:47:35 -0500

    mercado libre

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public'))); // Se hace la carpeta public publica
-app.use(express.static(path.join(__dirname, './views'))); // Se hace la carpeta public publica
-
 app.get('/mostrar_mensaje', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/home.html')); // Permite enviar un archivo HTML
+res.sendFile(path.resolve(__dirname, './views/home.html')); // Permite enviar un archivo HTML
 });
 
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './views')));
 
-app.get('*',(req,res)=>{
-    res.send("Ruta restringida, hemos localizado la dirección de su computador");
+/*
+app.listen(3002, () => {
+console.log("Servidor corriendo");
 });
 
+*/
 
-app.listen(process.env.PORT || 3000 , function () {
-    console.log("Servidor corriendo");
-});
+app.listen(process.env.PORT || 3000, function() {
+console.log("Servidor corriendo");
+})
+
+
