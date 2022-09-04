e10a560dac6535c72394d3fcf1bc3ffbd27df927
JulianaJt <juliana.trujillov@urosario.edu.co>
2022-09-03 23:35:05 -0500

    mercado libre

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ app.use(express.static(path.join(__dirname, './public'))); // Se hace la carpeta
 app.use(express.static(path.join(__dirname, './views'))); // Se hace la carpeta public publica
 
 app.get('/mostrar_mensaje', (req,res) =>{
-res.sendFile(path.resolve(__dirname, './views/home.html')); // Permite enviar un archivo HTML
+    res.sendFile(path.resolve(__dirname, './views/home.html')); // Permite enviar un archivo HTML
 });
 
-app.get('*',(req,res)=>{
-    res.send("Ruta restringida, hemos localizado la dirección de su computador");
-});
 
 //app.use(express.static(path.resolve(__dirname, './views')));
 
