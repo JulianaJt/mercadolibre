69565c53fec85aaa8cc2c1dc116fc185f782224f
JulianaJt <juliana.trujillov@urosario.edu.co>
2022-09-05 18:30:15 -0500

    imagenes

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ console.log("Servidor corriendo");
 
 */
 
-app.listen(process.env.PORT || 3002, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Servidor corriendo");
 });
