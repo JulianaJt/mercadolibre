68cde377e02804fa0227022fd16c7c869ec2aea1
Jorge <jorgecuadradovelasquez@gmail.com>
2022-09-04 22:25:01 -0500

    cambios

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
+console.log("current directory", __dirname);
 
-app.get('/mostrar_mensaje', (req,res) =>{
-res.sendFile(path.resolve(__dirname, './views/home.html')); // Permite enviar un archivo HTML
+app.get("/mostrar_mensaje", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html")); // Permite enviar un archivo HTML
 });
 
-app.use(express.static(path.resolve(__dirname, './public')));
-app.use(express.static(path.resolve(__dirname, './views')));
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./views")));
 
 /*
 app.listen(3002, () => {
@@ -17,8 +17,6 @@ console.log("Servidor corriendo");
 
 */
 
-app.listen(process.env.PORT || 3001, function() {
-console.log("Servidor corriendo");
-})
-
-
+app.listen(process.env.PORT || 3001, function () {
+  console.log("Servidor corriendo");
+});
